8517794d355eb27b47db367afaa2ffebbaf55a8a
kmalio98 murillo conrado <kannermc@gmail.com>
2022-09-21 18:41:12 -0500

    10977

---
+++ online judge.kaner murillo/c++/10977.cpp
@@ -0,0 +1,19 @@
+//https://onlinejudge.org/external/109/10977.pdf
+//10977
+
+#include<cstdio>
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+
+    int n , m;
+    
+    while(scanf("%i %i", &n, &m) && n!=0 || m!=0){
+
+        
+    }
+    return 0;
+}
