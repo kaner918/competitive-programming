e6a04437a203671037217dc019bff3a3ae1ad461
kaner918 <kannermc@gmail.com>
2022-09-23 00:55:50 -0500

    archivo segundo proyecto introduccion a la programación

---
+++ online judge.kaner murillo/python/449.py
@@ -0,0 +1,14 @@
+#https://onlinejudge.org/external/4/449.pdf
+#449
+
+from sys import stdin
+
+cad = stdin.readline().strip()
+
+while cad != "":
+
+    cad2 = stdin.readline().strip().split(";")
+
+
+
+    cad = stdin.readline().strip()
