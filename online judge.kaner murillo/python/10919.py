8d0f9fb4d444c20aa34cb704903b1efa87eec698
kaner918 <kannermc@gmail.com>
2022-10-04 10:27:18 -0500

    ejercicio 10919 resuelto

---
+++ online judge.kaner murillo/python/10919.py
@@ -0,0 +1,43 @@
+#https://onlinejudge.org/external/109/10919.pdf
+#10919
+
+from sys import stdin
+
+prerequisites = stdin.readline().split()
+
+while len(prerequisites) > 1:
+
+    courses = []
+    ans = True
+
+    while len(courses) < int(prerequisites[0]):
+
+        courses.extend([int(x) for x in stdin.readline().split()])
+
+    for i in range(int(prerequisites[1])):
+
+        category = [int(x) for x in stdin.readline().split()]
+        n = 2
+        counter = 0
+
+        while n < len(category) and counter < category[1]:
+
+            if category[n] in courses:
+
+                counter+=1
+
+            n+=1
+        
+        if counter < category[1]:
+
+            ans = False
+
+    if ans == True:
+
+        print("yes")
+    
+    else:
+
+        print("no")
+
+    prerequisites  = stdin.readline().split()
