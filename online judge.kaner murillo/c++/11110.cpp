914d3cb012ada414087f6e2d00c64cd8464e8e1e
kaner918 <kannermc@gmail.com>
2022-10-29 18:21:12 -0500

    adelanto del 11110

--- online judge.kaner murillo/c++/11110.cpp
+++ online judge.kaner murillo/c++/11110.cpp
@@ -3,18 +3,44 @@
 
 #include<cstdio>
 #include<iostream>
+#include<map>
 
 using namespace std;
 
-int graph[100][100], visits[100][100];
+int graph[100][100], visits[100][100], arrRow[] = {1, 0, -1, 0}, arrColum[] = {0, -1, 0, 1};
+
+void dfs(int row, int colum, int size){
+
+    visits[row][colum] = 1;
+
+    int i, auxRow, auxColum;
+
+    for(i = 0; i<4; i++){
+
+        auxRow = row + arrRow[i];
+        auxColum = colum + arrColum[i];
+
+        if((auxRow > -1 && auxColum > -1) && (auxRow < size && auxColum < size) && graph[row][colum] == graph[auxRow][auxColum] && visits[auxRow][auxColum] == 0){
+
+            dfs(auxRow, auxColum, size);
+        }
+    }
+
+}
 
 int main(){
 
     int size, n, m, i, x;
+    bool ans;
 
     while(scanf("%i", &size) && size !=  0){
 
+        ans = true;
+        map<int, bool>comp;
+
         for(n = 0; n<size; n++){
+
+            comp[n+1] = false;
 
             for(m = 0; m<size; m++){
 
@@ -33,14 +59,34 @@ int main(){
             }
         }
 
-        for(n = 0; n<size; n++){
+        for(n = 0; n<size && ans; n++){
 
-            for(m = 0; m<size; m++){
+            for(m = 0; m<size && ans; m++){
 
-                cout<<graph[n][m]<<" ";
+                if(visits[n][m] == 0){
+                    
+                    if(comp[graph[n][m]] == false){
+
+                        dfs(n, m, size);
+                        comp[graph[n][m]] = true;
+                    } 
+
+                    else{
+
+                        ans = false;
+                    }
+                }
             }
+        }
 
-            cout<<endl;
+        if(ans){
+
+            printf("good\n");
+        }
+
+        else{
+
+            printf("wrong\n");
         }
     }
 
