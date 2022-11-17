29332137798f4dbb7cf1476b15baff39122b6988
kaner918 <kannermc@gmail.com>
2022-11-16 21:46:06 -0500

    ejercicio 1112 resuleto

---
+++ online judge.kaner murillo/c++/1112.cpp
@@ -0,0 +1,97 @@
+//https://onlinejudge.org/external/11/1112.pdf
+//1112
+
+#include<climits>
+#include<queue>
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+int points;
+vector<int>cost(100);
+
+void init(int point){
+
+    int i;
+
+    for(i = 0; i<points; i++){
+
+        cost[i] = INT_MAX;
+    }
+
+    cost[point] = 0;
+}
+
+void dijkstra(int point, vector<vector<pair<int, int>>>&graph){
+
+    int i, vertex, vertexAd, auxCost, weight;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>cola;
+
+    init(point);
+
+    cola.push(make_pair(point, 0));
+
+    while(!cola.empty()){
+
+        vertex = cola.top().first;
+        auxCost = cola.top().second;
+
+        cola.pop();
+    
+        if(auxCost == cost[vertex]){
+
+            for(i = 0; i<graph[vertex].size(); i++){
+
+                vertexAd = graph[vertex][i].first;
+                weight = graph[vertex][i].second;
+
+                if(cost[vertex] != INT_MAX && auxCost + weight < cost[vertexAd]){
+
+                    cost[vertexAd] = auxCost + weight;
+                    cola.push(make_pair(vertexAd, cost[vertexAd]));
+                }
+            }
+        }
+    }
+}
+
+int main(){
+
+    int cases, i, mouseA, mouseB, conections, finish, time, weight, counter; 
+
+    scanf("%i", &cases);
+
+    while(cases--){ 
+
+        cin.ignore();
+
+        scanf("%i %i %i %i", &points, &finish, &time, &conections);
+        counter = 0;
+        vector<vector<pair<int, int>>>graph(points);
+
+        while(conections--){
+
+            scanf("%i %i %i", &mouseA, &mouseB, &weight);
+            graph[mouseA-1].push_back(make_pair(mouseB-1, weight));
+        }
+
+        for(i = 0; i<points; i++){
+
+            dijkstra(i, graph);
+
+            if(cost[finish-1] <= time){
+
+                counter++;
+            }
+        }
+
+        printf("%i\n", counter);
+
+        if(cases>0){
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
