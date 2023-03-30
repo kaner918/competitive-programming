bd056a036b6283587d4a4f5e436e88006840459f
kmalio98 murillo conrado <kannermc@gmail.com>
2023-03-30 17:27:35 -0500

    problema 1216 iniciado

---
+++ online judge.kaner murillo/c++/1216.cpp
@@ -0,0 +1,26 @@
+//https://onlinejudge.org/external/12/1216.pdf
+//1216
+
+#include<cstdio>
+#include<iostream>
+#include<vector>
+#include<map>
+#include<climits>
+
+using namespace std;
+
+int main(){
+
+    int cases, receiver;
+
+    scanf("%i", &cases);
+
+    while(cases--){
+
+        scanf("%i", &receiver);
+
+
+    }
+
+    return 0;
+}
