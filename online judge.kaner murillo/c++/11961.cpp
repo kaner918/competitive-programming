ec209bbd01811d7b61b95b4653dd188552a07264
kmalio98 murillo conrado <kannermc@gmail.com>
2023-05-17 10:47:43 -0500

    terminar de corregir el problema 11961

--- online judge.kaner murillo/c++/11961.cpp
+++ online judge.kaner murillo/c++/11961.cpp
@@ -12,17 +12,15 @@ using namespace std;
 
 char arr[] = {'A', 'C', 'G', 'T'};
 
-void solved(int limit, int change, map<vector<char>, int>&memo, vector<vector<char>>&res, vector<char>&comb){
+void solved(int limit, int change, int index, map<vector<char>, int>&memo, vector<vector<char>>&res, vector<char>&comb){
 
+    if(index == comb.size()){
 
-    if(change == limit){
-
-        auto it = memo.find(comb);
-
-        if(it == memo.end()){
+        /* auto it = memo.find(comb); */
+        if(change == limit){
 
             res.push_back(comb);
-            memo[comb] = 1;
+            /* memo[comb] = 1; */
         }
     }
 
@@ -31,17 +29,21 @@ void solved(int limit, int change, map<vector<char>, int>&memo, vector<vector<ch
         int i, n;
         char pre;
         
-        /* solved(index+1, res, comb); */
+        solved(limit, change, index+1, memo, res, comb);
+        
+        for(i = 0; i<comb.size(); i++){
 
-        for(i = 0; i<4; i++){
+            comb[i] = arr[0];
+            solved(limit, change+1, index+1, memo, res, comb);
 
-            for(n = 0; n<comb.size(); n++){
+            comb[i] = arr[1];
+            solved(limit, change+1, index+1, memo, res, comb);
 
-                pre = comb[n];
-                comb[n] = arr[i];
-                solved(limit, change+1, memo, res, comb);
-                comb[n] = pre;
-            }
+            comb[i] = arr[2];
+            solved(limit, change+1, index+1, memo, res, comb);
+
+            comb[i] = arr[3];
+            solved(limit, change+1, index+1, memo, res, comb);
         }
     }
 }
@@ -65,13 +67,13 @@ int main(){
             comb.push_back(cad[i]);
         }
         map<vector<char>, int>memo;
-
-        solved(power, 0, memo, res, comb);
-        sort(res.begin(), res.end());
+        
+        solved(power, 0, 0, memo, res, comb);
+        /* sort(res.begin(), res.end()); */
 
         cout<<res.size()<<endl;
 
-        for(i = 0; i<res.size(); i++){
+        for(i = 1; i<res.size(); i++){
 
             for(n = 0; n<res[i].size(); n++){
                 cout<<res[i][n];
