50227a14d9c17171c3927541ca8e7827de310314
kaner918 <kannermc@gmail.com>
2023-08-10 22:57:18 -0500

    problem 12266 solved

--- online judge.kaner murillo/python/12266.py
+++ online judge.kaner murillo/python/12266.py
@@ -35,8 +35,6 @@ def main():
     for i in range(cases):
 
         operations = int(stdin.readline())
-        low_sell = -1
-        hight_buy = -1
         trans = -1
 
         sells = []
@@ -52,82 +50,44 @@ def main():
             else:
                 heappush(sells, Seller(int(op[1]), int(op[-1])))
 
-            if(len(buys)):
-                if hight_buy == -1:
-                    hight_buy = buys[0].price
+            while len(sells) and len(buys) and buys[0].price >= sells[0].price:
 
-                else:
-                    hight_buy = max(buys[0].price)
+                trans = sells[0].price
 
-            if(len(sells)):
-
-                if low_sell == -1:
-                    low_sell = sells[0].price
-
-                else:
-                    low_sell= max(sells[0].price)
-
-                
-            
-            if(len(buys) and len(sells)) and buys[0].price >= sells[0].price:
-
-                if(trans == -1):
-                    trans = sells[0].price
-                
-                else:
-                    trans = min(trans, sells[0].price)
-                
                 if buys[0].size > sells[0].size:
-                    buys[0].size-=sells[0].size
+
+                    buys[0].size-= sells[0].size
                     heappop(sells)
-                    low_sell = -1
-                    
-                    if(len(sells)):
-                        
-                        low_sell = sells[0].price
 
                 elif buys[0].size < sells[0].size:
-                    sells[0].size-=buys[0].size
+
+                    sells[0].size-= buys[0].size
                     heappop(buys)
-                    hight_buy = -1
-                    
-                    if(len(buys)):
-                        
-                        hight_buy = buys[0].price
+
                 else:
-                    
+
                     heappop(sells)
                     heappop(buys)
-                    hight_buy = -1
-                    low_sell = -1
-
-                    if(len(sells)):
-                        
-                        low_sell = sells[0].price
                     
-                    if(len(buys)):
-                        
-                        hight_buy = buys[0].price
 
-            if low_sell == -1:  
+            if not len(sells):  
 
                 print("-", end=" ")
             
             else:
-                print(low_sell, end=" ")
+                print(sells[0].price, end=" ")
 
-            if hight_buy == -1:
+            if not len(buys):
 
                 print("-", end=" ")
             
             else:
-                print(hight_buy, end=" ")
+                print(buys[0].price, end=" ")
             
             if trans == -1:
                 print("-")
             
             else:
                 print(trans)
-
 
 main()
