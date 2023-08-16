76d8ed99eeab3fd5f1d5ea7894b456cb1038a1e0
kaner918 <kannermc@gmail.com>
2023-08-15 22:05:32 -0500

    problema 11876 adalantado

--- online judge.kaner murillo/python/11876.py
+++ online judge.kaner murillo/python/11876.py
@@ -1,12 +1,14 @@
 #https://onlinejudge.org/external/118/11876.pdf
 #11876
 
-from math import sqrt
+from math import sqrt, floor
 from sys import stdin
+from collections import deque
 
 def main():
 
     cases = int(stdin.readline())
+    numbers = deque([1])
     ans = 0
 
     for i in range(cases):
@@ -14,16 +16,23 @@ def main():
         a, b = [int(x) for x in stdin.readline().split()]
         ans = 0
 
-        while a <= b:
+        while numbers[-1] < b:
 
-            print("numero:", a)
             counter = 0
-            for n in range(1, a+1):
+            n = 1
+            while n <= sqrt(numbers[-1]):
             
-                counter+= not (a % n)
+                counter+= (not (numbers[-1] % n))*2
+                n+=1
 
-            a+=counter
-            ans+=1
-        print(f"Case {i+1}: {ans}")
+            if int(sqrt(numbers[-1]))**2 == numbers[-1]:
+                counter-=1
+
+            numbers.append(numbers[-1] + counter)
+
+        print(len(numbers))
+        print(numbers)
+
+        """ print(f"Case {i+1}: {ans}") """
         
 main()
