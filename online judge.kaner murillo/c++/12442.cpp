0df5e37ae331c56ddee66d5b029f250cc9e6d3c3
kmalio98 murillo conrado <kannermc@gmail.com>
2023-09-29 19:08:56 -0500

    fordwars emails solved

--- online judge.kaner murillo/c++/12442.cpp
+++ online judge.kaner murillo/c++/12442.cpp
@@ -5,73 +5,105 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<map>
+#include<algorithm>
 
 using namespace std;
-int counter, index;
 
-int dfs(int v, int low, int component, vector<vector<int>>&graph, vector<bool>&components, vector<bool>&visits, vector<int>sizes){
+int index, routers;
+vector<int>visits(50001);
+vector<pair<int, int>>low(50001);
+vector<int>roots(50001);
+map<pair<int, int>, int>maxis;
+int dfs(int node, vector<vector<int>>&graph){
 
-    int i, nodes = 1;
-    visits[v] = low;
-    components[v] = component;
+    visits[node] = index;
+    low[node] = {node, index};
+    roots[node] = routers;
 
-    for(i = 0; i< graph[node].size(); i++){
-        
+    index++;
+    
+    int i, counter = 1, aux;
+
+    for(i = 0; i<graph[node].size(); i++){
+
         if(!visits[graph[node][i]]){
-            nodes+=dfs(graph[node][i], low + 1, graph, visits, sizes);        
+            aux = dfs(graph[node][i], graph);
+            if(low[node].second > low[graph[node][i]].second){
+                low[node] = low[graph[node][i]];
+            }
+            counter+=aux;
+        }
+
+        else if(roots[node] == roots[graph[node][i]]){
+            if(low[node].second > low[graph[node][i]].second){
+                low[node] = low[graph[node][i]];
+            }
+        }
+        
+        else{   
+
+            if(!maxis[{node, roots[graph[node][i]]}]){
+                
+                maxis[{node, roots[graph[node][i]]}] = visits[low[graph[node][i]].first];
+                counter+=maxis[{node, roots[graph[node][i]]}];
+            }
+
+            else if(visits[low[graph[node][i]].first] > maxis[{node, roots[graph[node][i]]}]){
+
+                counter-=maxis[{node, roots[graph[node][i]]}];
+                maxis[{node, roots[graph[node][i]]}] = visits[low[graph[node][i]].first];
+                counter+=maxis[{node, roots[graph[node][i]]}]; 
+            }
         }
     }
 
-    return nodes;
+    if(low[node].first == node){
+        visits[node] = counter;
+    }
+
+    return counter;
 }
 
 int main(){
 
-    int cases, martians, higer, martian, i, n, m, u, v;
+    int cases, martians, aux, higer, martian, i, n, m, u, v;
 
     scanf("%i", &cases);
 
     for(i = 1; i<cases+1; i++){
 
         scanf("%i", &martians);
-        vector<vector<int>> grafo(martians);
-        vector<vector<int>> flags(martians, vector<int>(martians, 0));
-
-        vector<bool>visits;
-        vector<int>components(martians, 0);
+        vector<vector<int>> graph(martians);
         vector<int>sizes(martians, 0);
+        maxis.clear();
 
         higer = 0;
         martian = 0;
-        index = 0;
+        index = 1;
+        routers = 0;
 
         for(n = 0; n<martians; n++){
 
             scanf("%i %i", &u, &v);
-            grafo[u-1].push_back(v-1);
-            visits.push_back(false);
+            graph[u-1].push_back(v-1);
+            visits[n] = 0;
+            roots[n] = -1;
         }
 
         for(n = 0; n<martians; n++){
             
-            if(visits[n] == false){
+            if(!visits[n]){
 
-                counter = 0;
-                index++;
-                dfs(n, grafo, visits, visits2);
+                routers++;
+                index = 1;
+                aux = dfs(n, graph);
 
-                components[index] = counter;
+                if(aux > higer){
 
-                if(counter > higer){
-
-                    higer = counter;
+                    higer = aux;
                     martian = n;
                 }
-
-                else if(counter == higer && n<martian){
-                    martian  = n;
-                }
-
             }
         }
 
