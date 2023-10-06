63101e3c0a0848b73363588134b5d338fd238ddf
kmalio98 murillo conrado <kannermc@gmail.com>
2023-10-06 18:17:58 -0500

    problem 13303 solved

--- online judge.kaner murillo/c++/13303.cpp
+++ online judge.kaner murillo/c++/13303.cpp
@@ -12,223 +12,123 @@
 
 using namespace std;
 
-//lecture
-map<string, int>init;
-map<string, int>finish;
-map<int, int>counters;
 map<string, int>index;
 map<int, string>index2;
-map<int, vector<pair<int, int>>>letters;
+map<string, int>counters;
+map<string, map<string, int>>letters;
+map<string, int>visits;
 
-//solved
-
-int indexC, indexTps;
-vector<int>tps(100001);
-vector<int>indexComponent(100001);
-vector<int>visits(100001);
-
-void kosarajuAux(int node, vector<vector<int>>&graph, vector<vector<int>>&graphT){
+void dfs(string node, vector<int>&pre){
 
     if(!visits[node]){
+        /* cout<<node<<endl; */
         visits[node] = 1;
-        int i, mini = INT_MAX, size = 0;
+        int i, size = 0;
+        string mini = ""; 
+        for(auto it = letters[node].begin(); it!= letters[node].end(); it++){
+            if(it->second == size){
+                if(counters[it->first] == counters[mini]){
 
-        for(i = 0; i<letters[node].size(); i++){
-            if(letters[node][i].second == size){
-
-                if(counters[letters[node][i].first] == counters[mini]){
-
-                    if(index2[letters[node][i].first] < index2[mini]){
-                        mini = letters[node][i].first;
-                        size = letters[node][i].second;
+                    if(it->first < mini){
+                        mini = it->first;
+                        size = it->second;
                     }
                 }
 
-                else if(counters[letters[node][i].first] > counters[mini]){
-                    mini = letters[node][i].first;
-                    size = letters[node][i].second;
+                else if(counters[it->first] > counters[mini]){
+                    mini = it->first;
+                    size = it->second;
                 }
             }
 
-            else if(letters[node][i].second > size){
-                mini = letters[node][i].first;
-                size = letters[node][i].second;
+            else if(it->second > size){
+                mini = it->first;
+                size = it->second;
             }
         }
-        if(mini != INT_MAX){
-            graph[node].push_back(mini);
-            graphT[mini].push_back(node);
-            kosarajuAux(mini, graph, graphT);
-        }
-
-        tps[indexTps--] = node;
-    }
-}
-
-void asing(int u, int v, vector<vector<int>>&graphT){
-    indexComponent[u] = v;
-    int i;
-
-    for(i = 0; i<graphT[u].size(); i++){
-        if(indexComponent[graphT[u][i]] == -1){
-            asing(graphT[u][i], v, graphT);
+        if(mini != ""){
+            pre[index[mini]] = index[node];
+            dfs(mini, pre);
         }
     }
-}
-
-void kosaraju(int size, vector<vector<int>>&graph, vector<vector<int>>&graphT){
-
-    int i;
-
-    for(i = 0; i<size; i++){
-        kosarajuAux(i, graph, graphT);
-    }
-
-    for(i = 0; i<size; i++){
-        if(indexComponent[tps[i]] == -1){
-            indexC++;
-            asing(tps[i], tps[i], graphT);
-        }
-    }
-}
-
-void dfs(int node, vector<vector<int>>&graph){
-
-    if(!visits[node]){
-        visits[node] = 1;
-        int i;
-
-        for(i = 0; i<graph[node].size(); i++){
-            dfs(graph[node][i], graph);
-        }
-    }
-}
-
-bool comp(pair<int, int>&a, pair<int, int>&b){
-
-    if(a.second == b.second){
-        if(counters[a.first] == counters[b.first]){
-            return index2[a.first] < index2[b.first];
-        }
-        return counters[a.first] > counters[b.first];
-    }
-    return a.second>b.second;
-}
-
-bool comp2(pair<string, int>&a, pair<string, int>&b){
-
-    if(a.second == b.second){
-        return a.first < b.first;
-    }
-
-    return a.second>b.second;
 }
 
 int main(){
 
-    int size, i, j, counter, counter2, flag;
-    string word, word2, line;
-    map<string, int>wordx;
-
+    int size, i, j, counter;
+    string word1, word2, line;
     while(cin>>size){
+        
         cin.ignore();
-        counter = 0;
-        init.clear(); // elements that initialit the lines
-        finish.clear(); //elements that finish the lines
-        index.clear();
-        index2.clear();
-        counters.clear();
-        letters.clear();
+        // index.clear();
+        // index2.clear();        
+        // counters.clear();
+        // letters.clear();
+        // visits.clear();
+        index = map<string, int>();
+        index2 = map<int, string>();
+        counters = map<string, int>();
+        letters = map<string, map<string, int>>();
+        visits = map<string, int>();
+        counter = 2;
+        counters["A"] = size;
+        counters["B"] = size;
+        index["A"] = 0;
+        index["B"] = 1;
+        index2[0] = "A";
+        index2[1] = "B";
 
         for(i = 0; i<size; i++){
             getline(cin, line);
             stringstream stk(line);
-            stk>>word;
-            flag = 0;
-            init[word]++;
-            if(!index.count(word)){
-                index[word] = counter;
-                indexComponent[counter] = -1;
-                tps[counter] = -1;
-                visits[counter] = 0;
-                index2[counter++] = word;
+
+            stk>>word1;
+
+            if(!index.count(word1)){
+                index[word1] = counter;
+                index2[counter++] = word1;
             }
-            counters[index[word]]++;
-            wordx[word] = 1;
-            word2 = word;
+            counters[word1]++;
+            letters["A"][word1]++;
+            word2 = word1;
+
             while(stk>>word2){
-                wordx[word2] = 1;
+
                 if(!index.count(word2)){
                     index[word2] = counter;
-                    indexComponent[counter] = -1;
-                    tps[counter] = -1;
-                    visits[counter] = 0;
                     index2[counter++] = word2;
-                    
                 }
-                counters[index[word2]]++;
-                flag = 0;
-                for(j = 0; j<letters[index[word]].size() && !flag; j++){
-                    if(letters[index[word]][j].first == index[word2]){
-                        letters[index[word]][j].second++;
-                        flag = 1;
-                    }
-                }
-
-                if(!flag){
-                    letters[index[word]].push_back({index[word2], 1});
-                }
-                word = word2;
+                counters[word2]++;
+                letters[word1][word2]++;
+                word1 = word2;
             }
 
-            flag = 0;
-            finish[word]++;
-        }   
+            letters[word2]["B"]++;       
+        }        
 
-        for(auto it = wordx.begin(); it!=wordx.end(); it++){
-            cout<<it->first<<" ";
-        }
-        cout<<endl;
-        vector<pair<string, int>> init2(init.begin(), init.end());
-        vector<pair<string, int>> finish2(finish.begin(), finish.end());
-        sort(init2.begin(), init2.end(), comp2);
-        sort(finish2.begin(), finish2.end(), comp2);
-        /* for(auto it = letters.begin(); it!=letters.end(); it++){
+        vector<int>pre(counter+1);
+        vector<string>res;
+        dfs("A", pre);
 
-            cout<<"hi mr: "<<index2[it->first]<<endl;
-
-            for(i = 0; i<it->second.size(); i++){
-                cout<<index2[it->second[i].first]<<" "<<it->second[i].second<<endl;
-            }
-        } */
-
-        /* vector<vector<int>>graph(counter);
-        vector<vector<int>>graphT(counter);
-        indexC = 0;
-        indexTps = counter-1;
-
-        kosaraju(counter, graph, graphT);
-        for( i = 0; i<counter; i++){
-            visits[i] = 0;
-        }
-
-        counter2 = 0;
-     
-        dfs(index[init2[0].first], graph);
-        
-        if(indexComponent[index[init2[0].first]] != indexComponent[index[finish2[0].first]] && !visits[index[finish2[0].first]]){
+        if(!visits["B"]){
             cout<<"INFINITE"<<endl;
         }
 
         else{
-            cout<<init2[0].first;
-            word = init2[0].first;
-            while(word != finish2[0].first){
-                word = index2[graph[index[word]][0]];
-                cout<<" "<<word;
+            
+           
+            word1 = index2[pre[1]];
+            while(word1 != "A"){
+                res.push_back(word1);
+                word1 = index2[pre[index[word1]]];
+            }
+
+            cout<<res[res.size()-1];
+            for(i = res.size()-2; i>-1; i--){
+                cout<<" "<<res[i];
             }
             cout<<endl;
-        } */
+        }
     }
 
     return 0;
