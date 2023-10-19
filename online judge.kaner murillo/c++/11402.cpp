1fd1f7b183c847af36ebf6e093d2e7c927cfbf7f
kmalio98 murillo conrado <kannermc@gmail.com>
2023-10-19 17:59:40 -0500

    11402 iniciado

---
+++ online judge.kaner murillo/c++/11402.cpp
@@ -0,0 +1,23 @@
+//https://onlinejudge.org/external/114/11402.pdf
+//11402
+
+#include<cstdio>
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+vector<int>A(1024001);
+vector<int>tree(1024000*2);
+
+int main(){
+
+    int cases, size, i, j;
+    char consult;
+    string cad;
+
+    scanf("%i", &cases);
+
+    return 0;
+}
